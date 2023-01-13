7f9a5fe3bd49c6322af051e4b4ac57343b28e4cf
ErwanFagnou <r1bengalis@gmail.com>
2023-01-13 22:37:27 +0100

    ESM-2 trainable

--- models/ESM2.py
+++ models/ESM2.py
@@ -24,12 +24,13 @@ class ESM2(BaseProteinModel):
             batch_size=16,
             num_validation_samples=100,
             optimizer=torch.optim.Adam,
-            optimizer_kwargs=dict(lr=2e-3, betas=(0.9, 0.98), weight_decay=1e-2),
+            optimizer_kwargs=dict(lr=2e-3),
             # lr_scheduler=torch.optim.lr_scheduler.CosineAnnealingLR,
             # lr_scheduler_kwargs=dict(T_max=200, eta_min=1e-5),
             lr_scheduler=torch.optim.lr_scheduler.ExponentialLR,
             lr_scheduler_kwargs=dict(gamma=pow(1e-2, 1/200)),
         )
+        self.output_dim = self.config.hidden_dim
 
         d = self.config.hidden_dim
 
