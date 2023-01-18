89e2ac8e35c87b4940a596c2669e77df0b59ead0
Denis Duval <denis.duval13@gmail.com>
2023-01-18 20:14:45 +0100

    random queries, nq = 10

--- models/MultiHeadAttention.py
+++ models/MultiHeadAttention.py
@@ -44,15 +44,15 @@ class MultiHeadAttention(BaseProteinModel):
         # self.aggregator = aggr.MeanAggregation()
 
         self.queries = torch.randn(self.config.num_queries, d)
-        self.device = None
+        self.dev = None
 
     def forward(self, sequences, graphs, return_embeddings=True, random_mask=False):
         node_features = graphs.x
         node_features = self.node_proj(node_features)
 
-        if not self.device:
-            self.device = node_features.device
-            self.queries = self.queries.to(self.device) 
+        if not self.dev:
+            self.dev = node_features.device
+            self.queries = self.queries.to(self.dev) 
         # Pad sequences
         idx_n = 0
         x, attn_mask = [], []
--- models/test.py
+++
@@ -1,6 +0,0 @@
-import torch
-
-r = torch.randn(3, 6)
-r = torch.linalg.qr(r)[0]
-
-print(r.shape)
