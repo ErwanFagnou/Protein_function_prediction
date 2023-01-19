37cf1a369fa6d5643c0040cd45b1b2f4df9a66dc
ErwanFagnou <r1bengalis@gmail.com>
2023-01-19 23:19:59 +0100

    reduced num_queries to 5

--- models/MultiHeadAttention.py
+++ models/MultiHeadAttention.py
@@ -30,13 +30,13 @@ class MultiHeadAttention(BaseProteinModel):
         super(MultiHeadAttention, self).__init__()
 
         self.config = ConfigDict(
-            name='ESM2_150M+MHA(d=128,h=4)+query=random+10queries+dropout=0.2+labelSmoothing=0.05+1of5layers',
+            name='ESM2_150M+MHA(d=128,h=4)+query=random+5queries+dropout=0.2+labelSmoothing=0.05+1of5layers',
             hidden_dim=128,
             num_layers=1,
             num_heads=4,
 
             dropout=0.2,
-            num_queries=10,
+            num_queries=5,
             epochs=200,
             batch_size=64,
             num_validation_samples=500,
