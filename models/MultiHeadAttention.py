587f1b617f4fcb66010efafdef28f7646fa4d970
ErwanFagnou <r1bengalis@gmail.com>
2023-01-19 21:40:24 +0100

    changed num_heads

--- models/MultiHeadAttention.py
+++ models/MultiHeadAttention.py
@@ -30,10 +30,10 @@ class MultiHeadAttention(BaseProteinModel):
         super(MultiHeadAttention, self).__init__()
 
         self.config = ConfigDict(
-            name='ESM2_35M+MHA(d=128,h=16)+query=random+10queries+dropout=0.5+labelSmoothing=0.1',
+            name='ESM2_35M+MHA(d=128,h=4)+query=random+10queries+dropout=0.5+labelSmoothing=0.1',
             hidden_dim=128,
             num_layers=1,
-            num_heads=16,
+            num_heads=4,
 
             dropout=0.5,
             num_queries=10,
