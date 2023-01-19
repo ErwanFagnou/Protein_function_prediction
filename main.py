f3ca52719b8a95201f8413ddea7acdc52888d36a
ErwanFagnou <r1bengalis@gmail.com>
2023-01-19 21:40:44 +0100

    changed gpu id

--- main.py
+++ main.py
@@ -60,7 +60,7 @@ def get_model(num_node_features):
 
 
 if __name__ == '__main__':
-    device_id = 1
+    device_id = 2
     device = torch.device(f"cuda:{device_id}" if torch.cuda.is_available() else "cpu")
 
     # Create models (pretrained encoder and classification model)
