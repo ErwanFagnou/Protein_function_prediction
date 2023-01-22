0c217dd728f8afa2869bafd589f7220ca0400932
ErwanFagnou <r1bengalis@gmail.com>
2023-01-22 01:35:16 +0100

    tests with ESM 3B

--- main.py
+++ main.py
@@ -69,13 +69,13 @@ def main_loop(model_num: int, model=None):
         model.config.name = f'{model_num}_{model.config.name}'
 
     # To load different configs
-    lr = [1e-4, 2e-4][model_num]
+    lr = [7e-5, 1e-4][model_num]
     print(f"lr: {lr}")
     model.config.optimizer_kwargs['lr'] = lr
     if model_num == 1:
-        model.LABEL_SMOOTHING = 0.1
-    if model_num == 2:
         model.LABEL_SMOOTHING = 0.02
+    if model_num == 2:
+        model.LABEL_SMOOTHING = 0.01
 
     if TRAIN_MODEL:
         train(model, protein_dataset, device, device_id, pretrained_seq_encoder=pretrained_seq_encoder)
